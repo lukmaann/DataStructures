a1af7af63f6838e68bdcec039f1461c2dbb754d2
lukmaan <lukmaannadaf@gmail.com>
2023-11-26 09:12:20 +0530

    modify

--- Arrays/practice/practice.java
+++ Arrays/practice/practice.java
@@ -23,55 +23,114 @@ public class practice {
 
     }
 
+    public static void ReverseTheArray(int array[]) {
+        int start = 0, end = array.length - 1;
 
-    public static void ReverseTheArray(int array[]){
-        int start=0,end=array.length-1;
-
-        while (start<end) {
-            int temp=array[start];
-            array[start]=array[end];
-            array[end]=temp;
+        while (start < end) {
+            int temp = array[start];
+            array[start] = array[end];
+            array[end] = temp;
 
             start++;
             end--;
-            
+
         }
         System.out.println();
     }
 
-
-    public static void PairTheArray(int array[]){
-        for(int i=0;i<=array.length-1;i++){
-            for(int j=i+1;j<=array.length-1;j++){
-                System.out.print("("+array[i]+","+array[j]+")");
+    public static void PairTheArray(int array[]) {
+        for (int i = 0; i <= array.length - 1; i++) {
+            for (int j = i + 1; j <= array.length - 1; j++) {
+                System.out.print("(" + array[i] + "," + array[j] + ")");
             }
-                    System.out.println();
+            System.out.println();
 
         }
-      
+
+    }
+
+    public static int removeDuplicate(int arr[]) {
+        int i = 0;
+        for (int j = 1; j < arr.length; j++) {
+            if (arr[i] != arr[j]) {
+                i++;
+                arr[i] = arr[j];
+            }
+        }
+        return i + 1;
+
+    }
+
+    public static int removeElement(int nums[], int val) {
+
+        int k = 0;
+
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=val){
+                nums[k]=nums[i];
+                k++;
+            }
+        }
+
+        return k;
+
+    }
+
+    public static int  maxSubarrayPrefix(int array[]){
+        int max=0;
+        int prefix[]=new int [array.length];
+        prefix[0]=array[0];
+        for(int i=1;i<prefix.length;i++){
+            prefix[i]=prefix[i-1]+array[i];
+        }
+
+        for(int i=0;i<array.length;i++){
+            for(int j=i+1;j<array.length;j++){
+                int currentsum=i==0?prefix[j]:prefix[j]-prefix[i-1];
+                if(max<currentsum){
+                    max=currentsum;
+                }
+
+              
+                System.out.println();
+            }
+            System.out.println();
+        }
+
+        return max;
+
     }
 
 
-    public static void PrintArray(int array[]){
+    public static int  kadanesMaxSuArray(int array[]){
+        int max=Integer.MIN_VALUE;
+        int currentsum=0;
+
         for(int i=0;i<array.length;i++){
-            System.out.print(array[i]+" ");
+            currentsum+=array[i];
+            if(currentsum<0){
+                currentsum=0;
+            }
+            if(max<currentsum){
+                max=currentsum;
+            }
+        }
+
+
+
+        return max;
+    }
+
+    public static void PrintArray(int array[]) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
         }
     }
 
     public static void main(String args[]) {
-        int array[] = new int[5];
+        int array[] = { -2,-3,4,-1,-2,1,5,-3};
 
-        for (int i = 0; i < array.length; i++) {
-
-            array[i] = i+1;
-
-        }
-        // PrintArray(array);
-
-        PairTheArray(array);
-
-
-       
+        System.out.println(maxSubarrayPrefix(array));
 
         
 
