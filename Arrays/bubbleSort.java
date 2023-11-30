c53d3c566e690be9b1bbe5c65f232aed47c4ed22
LNUAKX <lukmaannadaf@gmail.com>
2023-11-30 21:31:20 +0530

    modify

---
+++ Arrays/bubbleSort.java
@@ -0,0 +1,22 @@
+package Arrays;
+
+public interface bubbleSort {
+
+    
+
+
+    public static void printArray(int array[]){
+        for(int i=0;i<array.length;i++){
+            System.out.print(array[i]+" ");
+        }
+    }
+    public static void main(String[] args) {
+
+        int array[]={2,1,3,4,1,3,6,8,5,3};
+ 
+
+        printArray(array);
+
+        
+    }
+}
