136139920ccd7ee3d67cab22c97b8842de7209bc
LNUAKX <lukmaannadaf@gmail.com>
2023-12-16 14:12:39 +0530

    modify

--- Strings/ArrayOfStrings/Strings.java
+++ Strings/ArrayOfStrings/Strings.java
@@ -25,11 +25,22 @@ public class Strings {
     }
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the your name:");
-        String name = sc.nextLine();
-        System.out.println(Character.isLetterOrDigit(name.charAt(0)));
-        // System.out.println(palindrome(name));
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print("Enter the your name:");
+        // String name = sc.nextLine();
+        // System.out.println(Character.isLetterOrDigit(name.charAt(0)));
+        // // System.out.println(palindrome(name));
+
+
+        String strings[]={"apple","applq","a"};
+        String largest=strings[0];
+        for(int i=0;i<strings.length;i++){
+            if(largest.compareTo(strings[i])<0){
+                largest=strings[i];
+            }
+        }
+
+        System.out.print(largest);
     }
 
 }
