1f8754d02ebd30db7cba07f257de11cb7f060b73
LNUAKX <lukmaannadaf@gmail.com>
2023-12-17 12:59:09 +0530

    modify

--- Bitmanipulation/practice.java
+++ Bitmanipulation/practice.java
@@ -70,22 +70,23 @@ public class practice {
         return (n & (n - 1)) == 0;
     }
 
-   public static int countSetBits(int n){
-    int count=0;
+    public static int countSetBits(int n) {
+        int count = 0;
 
-    while (n>0) {
-        count+=n&1;
-        n>>=1;
-        
+        while (n > 0) {
+            count += n & 1;
+            n >>= 1;
+
+        }
+
+        return count;
     }
 
 
-
-    return count;
-   }
+    
 
     public static void main(String[] args) {
-        System.out.println(countSetBits(7));
+        System.out.println(~5);
     }
 
 }
