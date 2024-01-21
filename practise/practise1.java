fcc6aa1e6acae62af2805bdb3a2c64684b24e8a6
LNUAKX <lukmaannadaf@gmail.com>
2024-01-21 20:14:08 +0530

    modifY

--- practise/practise1.java
+++ practise/practise1.java
@@ -4,42 +4,53 @@ public class practise1 {
 
     public static void rotate(int[] nums, int k) {
 
-        int n=nums.length;
-        k=k%n;
+        int n = nums.length;
+        k = k % n;
 
-        reverse(nums, k, n-1);
-        reverse(nums,0,k-1);
-        reverse(nums, 0, n-1);
-
+        reverse(nums, k, n - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, 0, n - 1);
 
     }
 
-    public static void reverse(int array[], int start, int end){
+    public static void reverse(int array[], int start, int end) {
 
-        
-        while(start<end){
-            int temp=array[start];
-            array[start]=array[end];
-            array[end]=temp;
+        while (start < end) {
+            int temp = array[start];
+            array[start] = array[end];
+            array[end] = temp;
             start++;
             end--;
         }
     }
 
+    public static ArrayList<Integer> rotateArray(ArrayList<Integer> array, int k) {
+        ArrayList<Integer> temp = new ArrayList<>();
 
-    public static ArrayList<Integer> rotateArray(ArrayList<Integer> array,int k){
-        ArrayList<Integer> temp= new ArrayList<>();
-
-        for(int i=k;i<array.size();i++){
+        for (int i = k; i < array.size(); i++) {
             temp.add(array.get(i));
         }
-        for(int i=0;i<k;i++){
+        for (int i = 0; i < k; i++) {
             temp.add(array.get(i));
         }
 
         return temp;
 
- 
+    }
+
+    public static void movezeros(int nums[]) {
+
+       int k=0;
+
+       for(int i=0;i<nums.length;i++){
+        if(nums[i]!=0){
+            int temp=nums[i];
+            nums[i]=nums[k];
+            nums[k]=temp;
+            k++;
+        }
+       }
+
     }
 
     public static void printArray(int array[]) {
@@ -50,15 +61,11 @@ public class practise1 {
     }
 
     public static void main(String[] args) {
-        int array[] = { 1, 2, 3, 4, 5, 6, 7 };
-        ArrayList<Integer> arraylist= new ArrayList<>();
+        int array[] = { 0,1,2,3,4,5,6,7,8,9 };
 
-        for(int i=0;i<7;i++){
-            arraylist.add(i+1);
-        }
-
-        System.out.println(rotateArray(arraylist, 3));
-        
+        printArray(array);
+        movezeros(array);
+        printArray(array);
 
     }
 }
