a941f5a59e49941597d5de9e9511f4ab6ce9334f
LNUAKX <lukmaannadaf@gmail.com>
2024-05-22 07:47:38 +0530

    modify

--- module2/mediumArray/Hashmap.java
+++
@@ -1,5 +0,0 @@
-package module2.mediumArray;
-
-public class Hashmap<T1, T2> {
-
-}
--- module2/tempCodeRunnerFile.java
+++
@@ -1,6 +0,0 @@
-    // for(int i=0;i<mat.length;i++){
-        //     for(int j=0;j<mat[0].length;j++){
-                
-            
-        //     }
-        // }
